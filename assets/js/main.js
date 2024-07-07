e094e9baedb066f4695057a7204eb8ac8dc471a2
PurpleMariposa <148848993+PurpleMariposa@users.noreply.github.com>
2024-07-07 23:29:42 +0800

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,115 @@
+(function ($) {
+    "use strict";
+
+    /*:::::::::::::::::::::::::::::::::::
+            Navbar Area
+    :::::::::::::::::::::::::::::::::::*/
+
+     // Navbar Sticky
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+
+        if (scroll >= 1) {
+            $(".navbar").addClass("bg-primari");
+        } else {
+            $(".navbar").removeClass("bg-primari");
+        }
+    });
+
+
+    //Smoth Scroll
+    $(function () {
+        $('.nav-link, .smoth-scroll').on('click', function (event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top - 0
+            }, 1000);
+            event.preventDefault();
+        });
+    });
+
+    /*==========================
+        Hero Area Slider
+    ============================*/
+
+    $('.hero-area-slids').owlCarousel({
+        items: 1,
+        loop: true,
+        nav: false,
+        doots: false,
+        autoplay: true,
+        animateOut: 'fadeOutRight',
+        animateIn: 'fadeIn'
+
+    });
+    //Wow Animation
+    new WOW().init();
+    /*==========================
+        Hero Title typer
+    ============================*/
+    var element = $('.typed');
+
+    $(function () {
+        element.typed({
+            strings: ["Web Developer.", "UX Designer."],
+            typeSpeed: 100,
+            loop: true,
+            autoplay: true,
+        });
+    });
+
+    /*::::::::::::::::::::::::::::::::::::
+       Project Section
+    ::::::::::::::::::::::::::::::::::::*/
+
+    lightbox.option({
+        'imageFadeDuration': 800,
+        'resizeDuration': 500,
+        'wrapAround': true
+    });
+
+    $('.portfolio-area').mixItUp();
+
+
+    /*::::::::::::::::::::::::::::::::::::
+       Contact Area 
+    ::::::::::::::::::::::::::::::::::::*/
+    var form = $('#contact-form');
+
+    var formMessages = $('.form-message');
+    $(form).submit(function (e) {
+        e.preventDefault();
+        var formData = $(form).serialize();
+        $.ajax({
+                type: 'POST',
+                url: $(form).attr('action'),
+                data: formData
+            })
+            .done(function (response) {
+                $(formMessages).removeClass('error');
+                $(formMessages).addClass('success');
+                $(formMessages).text(response);
+
+                $('#contact-form input,#contact-form textarea').val('');
+            })
+            .fail(function (data) {
+                $(formMessages).removeClass('success');
+                $(formMessages).addClass('error');
+
+                if (data.responseText !== '') {
+                    $(formMessages).text(data.responseText);
+                } else {
+                    $(formMessages).text('Oops! An error occured and your message could not be sent.');
+                }
+            });
+    });
+    
+    
+    /*::::::::::::::::::::::::::::::::::::
+    Preloader
+    ::::::::::::::::::::::::::::::::::::*/
+    $(window).on('load', function () {
+        $('.preloader').fadeOut();
+    });
+
+}(jQuery));
